ab470a30ab667aca7b9b9ebef6dce97f37448de8
RegisAB <regis_ant@yahoo.com>
2017-02-10 16:06:32 -0500

    small changes in the two files

--- SalesTool/src/ca/demo/salestool/Main.java
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -14,6 +14,8 @@ public class Main {
 		System.out.println("THIS APP SHOWS SALES DATA");
 		
 		System.out.println("Test 1");
+		System.out.println("Test 2");
+		System.out.println("Test 3");
 	}
 
 }
--- SalesTool/src/ca/demo/salestool/SalesData.java
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -6,7 +6,7 @@ public class SalesData {
 	public void display(){
 		System.out.println("Data: ");
 		for(int i = 0; i < data.length; i++){
-			System.out.println("Next value: "+ data[i]);
+			System.out.println("Next value: "+ data[i]);///
 		}
 	}
 
