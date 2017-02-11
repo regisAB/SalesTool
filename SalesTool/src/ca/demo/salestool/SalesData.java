6835c3838f5af9646a026e04bdbf806df415f58d
RegisBKL <regis.a.bekale@gmail.com>
2017-02-10 19:30:36 -0500

    addided modification the first display message in salesdata file

--- SalesTool/src/ca/demo/salestool/SalesData.java
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -1,12 +1,12 @@
 package ca.demo.salestool;
 
 public class SalesData {
-	int data[] = {0, 4, 42};
+	int data[] = {0, 4, 42, 100};
 	
 	public void display(){
 		System.out.println("Header here Data: ");
 		for(int i = 0; i < data.length; i++){
-			System.out.println("Next value: "+ data[i]);///
+			System.out.println("Next value not to be pushed here: "+ data[i]);///
 		}
 	}
 
