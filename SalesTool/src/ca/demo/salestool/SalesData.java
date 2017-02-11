71b10d07481101b65e8982b0ad6c9d526a2680b3
prgmeur2 <boudepas@gmail.com>
2017-02-10 19:12:33 -0500

    added additional header display in the salesdata file

--- SalesTool/src/ca/demo/salestool/SalesData.java
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -1,10 +1,14 @@
 package ca.demo.salestool;
 
 public class SalesData {
-	int data[] = {0, 4, 42};
+	int data[] = {0, 4, 42, 57, 90, 100, 67};
 	
 	public void display(){
-		System.out.println("Header here Data: ");
+		//add three more headers
+		System.out.println("Header one added Data: ");
+		System.out.println("Header two  added Data: ");
+		System.out.println("Header three added Data: ");
+		System.out.println("Header here added Data: ");
 		for(int i = 0; i < data.length; i++){
 			System.out.println("Next value: "+ data[i]);///
 		}
